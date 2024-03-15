4933cecdd8c6d041479bab67c9c7ba864545744d
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-15 11:33:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "us-west-2" # Change to your desired AWS region
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" # Specify the desired version or version constraint here
+    }
+  }
+}
+
 module "iam_example_user" {
   source  = "./modules/iam"
   for_each = var.iam_users
