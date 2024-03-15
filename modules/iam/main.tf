072aa5a2cfb00006f9be173f1652253ae785ffa5
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-03-15 11:21:55 +0000

    Create main.tf

---
+++ modules/iam/main.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_user" "user" {
+  count = var.type == "user" ? 1 : 0
+  name  = var.name
+}
+
+resource "aws_iam_role" "role" {
+  count = var.type == "role" ? 1 : 0
+  name  = var.name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+}
+
+variable "name" {
+  type = string
+}
+
+variable "type" {
+  type = string
+  description = "Type of IAM entity to create: 'user' or 'role'."
+}
